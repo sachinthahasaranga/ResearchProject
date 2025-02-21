87e8217def47ad3adf6c5081a2f8220e2228dc03
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2025-02-21 08:04:33 +0530

    added all 4 categories but need to finalize one

--- frontend/src/pages/SelectCategory.jsx
+++ frontend/src/pages/SelectCategory.jsx
@@ -6,15 +6,11 @@ const SelectCategory = () => {
   // Array of 10 categories with the same background image
   const categories = [
     { id: 1, title: 'Finding Location', color: '#FF6B6B', description: 'This is the first category.', backgroundImage: 'images/listeningCategories/ironmanLocation.png' },
-    { id: 2, title: 'Category 2', color: '#4ECDC4', description: 'This is the second category.', backgroundImage: 'images/listeningCategories/ironmanLocation.png' },
-    { id: 3, title: 'Category 3', color: '#FFE66D', description: 'This is the third category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 4, title: 'Category 4', color: '#FF9F1C', description: 'This is the fourth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 5, title: 'Category 5', color: '#2EC4B6', description: 'This is the fifth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 6, title: 'Category 6', color: '#E71D36', description: 'This is the sixth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 7, title: 'Category 7', color: '#662E9B', description: 'This is the seventh category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 8, title: 'Category 8', color: '#F4A261', description: 'This is the eighth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 9, title: 'Category 9', color: '#2A9D8F', description: 'This is the ninth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
-    { id: 10, title: 'Category 10', color: '#E76F51', description: 'This is the tenth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
+    { id: 2, title: 'Conversation', color: '#4ECDC4', description: 'This is the second category.', backgroundImage: 'images/listeningCategories/Conversation.png' },
+    { id: 3, title: 'Voice Mail', color: '#FFE66D', description: 'This is the third category.', backgroundImage: 'images/listeningCategories/voiceMail.png' },
+    { id: 4, title: 'Story telling', color: '#FF9F1C', description: 'This is the fourth category.', backgroundImage: 'images/listeningCategories/storyTelling.png' },
+    { id: 5, title: 'Giving Instructions', color: '#2EC4B6', description: 'This is the fifth category.', backgroundImage: '../assets/listeningCategories/ironmanLocation.png' },
+   
   ];
 
   return (
