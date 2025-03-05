7dc6d3872cf909f180b1127cf52e9d161ac2a73d
Sachintha Hasaranga <sachinthahasaranga0415@gmail.com>
2025-03-05 19:32:10 +0530

    bug fix

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.DB_URI); 
         console.log('Database connected successfully');
     } catch (err) {
         console.error('Database connection error:', err.message);
-        process.exit(1); // Exit process with failure
+        process.exit(1); 
     }
 };
 
