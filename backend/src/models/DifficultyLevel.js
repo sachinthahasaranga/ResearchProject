2a974ed9b9e83da34c82da108a10c5f9cf9a0218
KavishiDSirisena <kavishisirisena177@gmail.com>
2025-03-19 10:01:23 +0530

    updated

--- backend/src/models/DifficultyLevel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const difficultyLevelSchema = new mongoose.Schema({
-    difficultyName: { type: String, required: true, unique: true },
-    difficultyWeight: { type: Number, required: true },
-    status: { type: Number, enum: [0, 1], default: 1 }, // 0 - inactive, 1 - active
-}, { timestamps: true });
-
-module.exports = mongoose.model("DifficultyLevel", difficultyLevelSchema);
--- backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../authentication/authController");
-
-router.post("/login", authController.loginUser);
-router.post("/register", authController.registerUser);
-
-module.exports = router;
